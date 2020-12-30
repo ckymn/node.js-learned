2dc4220bd70f1a37a200c8986ba7e447d36fa83e
Muhammet <muhammetcokyaman@gmail.com>
2020-12-30 17:23:39 +0300

    first commit

---
+++ Asenkron-Program/promise.js
@@ -0,0 +1,53 @@
+'use strict'
+const todos = [
+  { title: 'Tood 1', description: 'puresol bug temizligi..' },
+  { title: 'Tood 2', description: 'filmleri getir..' },
+  { title: 'Tood 3', description: 'video izle..' },
+  { title: 'Tood 4', description: 'play game..' }
+]
+
+let todoListEl = document.getElementById('todoList')
+
+const todoList = () => {
+  setTimeout(() => {
+    let todoItems = ''
+    todos.forEach(arr => {
+      todoItems += `
+                  <li>
+                      <b>${arr.title}</b>
+                      <p>${arr.description}</p>
+                  </li>
+              `
+      todoListEl.innerHTML = todoItems
+    })
+  }, 1000)
+}
+
+const newTodo = todo => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      todos.push(todo)
+      if (false) {
+        resolve()
+      } else {
+        reject()
+      }
+    }, 2000)
+  })
+}
+
+newTodo({
+  title: 'Todo 5',
+  description: 'Write codes'
+})
+  .finally(() => {
+    alert('Promise ready..')
+  })
+  .then(response => {
+    // eger resolve() calisirsa
+    todoList()
+  })
+  .catch(err => {
+    // eger reject() calisirsa
+    console.error('Hata var...')
+  })
