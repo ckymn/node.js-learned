71be76e94a81704df0337fb8765e948a85ddc071
ckymn <cokyamanmuhammet@gmail.com>
2021-04-08 22:49:43 +0300

    f🎉

--- Blog/middleware/authMiddleware.js
+++ Blog/middleware/authMiddleware.js
@@ -1,22 +1,22 @@
 const jwt = require("jsonwebtoken");
-const requireAuth = (req,res,next) =>{
-    // bu cookies'ta tokens kontrolunu cookie-parser yapiyor.
-    const token = req.cookies.tokens;
+const requireAuth = (req, res, next) => {
+  // bu cookies'ta tokens kontrolunu cookie-parser yapiyor.
+  const token = req.cookies.tokens;
 
-    if(token){
-        // token cozumleme
-        jwt.verify(token, "muhammet blog",(err,decodedToken) =>{
-            if(err){
-                console.error(err);
-                res.redirect("/login");
-            }else {
-                console.log(decodedToken);
-                next();
-            }
-        });
-    }else {
+  if (token) {
+    // token cozumleme
+    jwt.verify(token, "muhammet blog", (err, decodedToken) => {
+      if (err) {
+        console.error("token verify hatasi :", err);
         res.redirect("/login");
-    }
-}
+      } else {
+        console.log("token decoded : ", decodedToken);
+        next();
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+};
 
 module.exports = { requireAuth };
