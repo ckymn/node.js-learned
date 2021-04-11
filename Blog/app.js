65c89edfaa14f7cd7d8443b673fa7471c64a8c14
ckymn <cokyamanmuhammet@gmail.com>
2021-04-11 17:18:42 +0300

    fc

--- Blog/app.js
+++ Blog/app.js
@@ -9,6 +9,7 @@ const admin = require("./routers/adminRouter")
 const blog = require("./routers/blogRouter")
 const authRoute = require("./routers/authRouter")
 const {requireAuth,checkUser} = require("./middleware/authMiddleware");
+
 const app = express();
 
 //configure
